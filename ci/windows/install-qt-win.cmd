e6e453f8e4ac7cf962f6d74294651942172ade6a
tt2468 <tt2468@irltoolkit.com>
2021-04-11 20:17:04 -0700

    Update Qt Install URL

--- ci/windows/install-qt-win.cmd
+++ ci/windows/install-qt-win.cmd
@@ -1,5 +1,5 @@
 if not exist %QtBaseDir% (
-	curl -kLO http://home.tt2468.net:86/dl/Qt_5.15.2.7z -f --retry 5 -z Qt_5.15.2.7z
+	curl -kLO http://tt2468.net/dl/Qt_5.15.2.7z -f --retry 5 -z Qt_5.15.2.7z
     7z x Qt_5.15.2.7z -o%QtBaseDir%
 ) else (
 	echo "Qt is already installed. Download skipped."
